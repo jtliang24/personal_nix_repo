0452d6b55834680be6f215567a644ee6f13c9ddb
Jiatao Liang <jtliang24@gmail.com>
2025-07-31 12:45:47 -0400

    initial commit

---
+++ Artix_Game_Launcher.nix
@@ -0,0 +1,28 @@
+{
+  appimageTools,
+  fetchurl
+}:
+let
+  pname = "ArtixGameLauncher";
+  version = "2.20";
+  src = fetchurl {
+    url = "https://launch.artix.com/latest/Artix_Games_Launcher-x86_64.AppImage";
+    sha256 = "sha256-8eVXOm5g92wErWa6lbTXrCL04MWYlObjonHJk+oUI3E=";
+  };
+  appimageContents = appimageTools.extract {
+    inherit pname version src;
+  };
+in
+appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraPkgs = pkgs: [];
+
+  extraInstallCommands = ''
+    mkdir -p $out/share/applications
+    install -m 444 -D ${appimageContents}/ArtixGamesLauncher.desktop $out/share/applications/ArtixGamesLauncher.desktop
+    install -m 444 -D ${appimageContents}/ArtixLogo.png $out/share/icons/ArtixLogo.png
+  '';
+
+}
+
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        packages = rec {
+          ArtixGameLauncher = pkgs.callPackage ./Artix_Game_Launcher.nix {};
+          kando = pkgs.callPackage ./kando.nix {};
+          hello = pkgs.callPackage ./hello.nix {};
+          default = hello;
+        };
+        apps = rec {
+          ArtixGameLauncher = flake-utils.lib.mkApp { drv = self.packages.${system}.ArtixGameLauncher; };
+          kando = flake-utils.lib.mkApp { drv = self.packages.${system}.ArtixGameLauncher; };
+          hello = pkgs.callPackage ./hello.nix {};
+          default = hello;
+        };
+      }
+  
+  );
+}
