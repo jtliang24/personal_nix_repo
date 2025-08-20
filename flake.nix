df8d550bb8c5f9a803faaaa573221441ff5d0c64
Jiatao Liang <jtliang24@gmail.com>
2025-08-20 16:19:50 -0400

    nixfmt

--- flake.nix
+++ flake.nix
@@ -4,20 +4,27 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
     let
       # Define ArtixGameLauncher only for x86_64-linux
       artixSystem = "x86_64-linux";
       artixPkgs = import nixpkgs { system = artixSystem; };
-      ArtixGameLauncher = artixPkgs.callPackage ./Artix_Game_Launcher.nix {};
+      ArtixGameLauncher = artixPkgs.callPackage ./Artix_Game_Launcher.nix { };
     in
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in {
+      in
+      {
         packages = {
-          kando = pkgs.callPackage ./kando.nix {};
-          hello = pkgs.callPackage ./hello.nix {};
+          kando = pkgs.callPackage ./kando.nix { };
+          hello = pkgs.callPackage ./hello.nix { };
         };
 
         #apps = {
@@ -26,7 +33,8 @@
         #  };
         #};
       }
-    ) // {
+    )
+    // {
       # Add ArtixGameLauncher separately
       packages.${artixSystem}.ArtixGameLauncher = ArtixGameLauncher;
       #apps.${artixSystem}.ArtixGameLauncher = flake-utils.lib.mkApp {
